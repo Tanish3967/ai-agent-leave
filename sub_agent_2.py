bd740bbf29a9f90966cf2e8896b175ff0d249cb7
Tanish3967 <49781529+Tanish3967@users.noreply.github.com>
2025-03-15 15:47:24 +0530

    Update sub_agent_2.py

--- sub_agent_2.py
+++ sub_agent_2.py
@@ -1,14 +1,10 @@
 import autogen
-from agno.tools.exa import ExaTools
-
-EXA_API_KEY = "cf630edc-9843-4c86-8ac4-a55b830d9f34"
-exa_tools_api = ExaTools(api_key=EXA_API_KEY)
 
 class AcademicQueryAgent:
     def __init__(self):
         self.agent = autogen.AssistantAgent(
             name="AcademicQueryAgent",
-            system_message="Answers academic queries using ExaTools.",
+            system_message="Answers academic queries using Groq AI.",
             llm_config={"model": "deepseek-r1-distill-llama-70b"}
         )
 
